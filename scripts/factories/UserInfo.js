557d2f8b7ae609287b235537f65c80ec40b4cfd3
nDuy <106814633+nDuuy@users.noreply.github.com>
2022-09-13 04:46:46 +0100

    Add files via upload
    cleaning and begin of photographer work lightbox, like

--- scripts/factories/UserInfo.js
+++ scripts/factories/UserInfo.js
@@ -42,7 +42,6 @@ function photographerFactory(data) {
 		const Price = document.createElement('p');
 		Price.textContent = `${price}€/jour`;
 
-
 		// to create the elements
 		article.appendChild(LinkToUser);
 		article.appendChild(location);
@@ -64,6 +63,10 @@ function photographerFactory(data) {
 		const UserProfil = document.createElement('div');
 		UserProfil.className = 'Photographer-Profile';
 
+		// create div for name and tagline user 
+		const userInfo = document.createElement('div');
+		userInfo.className = 'info'
+
 		// create h1 for name 
 		const h1Name = document.createElement('h1');
 		h1Name.textContent = name;
@@ -71,15 +74,43 @@ function photographerFactory(data) {
 		// create and link p for city and country 
 		const location = document.createElement('p');
 		location.textContent = `${city}, ${country}`;
+		location.className = 'location-info'
 
 		// create and link p for tagline
 		const Tagline = document.createElement('p');
 		Tagline.textContent = tagline;
 
-		// to create the elements
-		UserProfil.appendChild(h1Name);
-		UserProfil.appendChild(location);
-		UserProfil.appendChild(Tagline);
+		// div for user img and contact btn
+		const imgDiv = document.createElement('div')
+		imgDiv.className = 'Btn-Img'
+
+		// create btn contact and ad onClick function 
+		const btnContact = document.createElement( 'button' )
+		btnContact.innerHTML = "Contactez-moi"
+		btnContact.className = "contact_button"
+		btnContact.addEventListener('click', function displayModal() {
+			const modal = document.getElementById("contact_modal");
+			modal.style.display = "block";
+		});
+
+		// img of user
+		const img = document.createElement('img');
+		img.setAttribute("src", picture);
+		img.setAttribute("alt", name);
+		img.className = 'Id-img';
+
+		// to create the elements on userprofile div
+		UserProfil.appendChild(userInfo)
+		UserProfil.appendChild(imgDiv)
+
+		// to create the elements on imgdiv
+		imgDiv.appendChild(img);
+		imgDiv.appendChild(btnContact)
+
+		// to create elements on di userInfo
+		userInfo.appendChild(h1Name);
+		userInfo.appendChild(location);
+		userInfo.appendChild(Tagline);
 
 		// for return our Div 
 		return (UserProfil);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,12 +10,21 @@ async function getData() {
 // show information of photographer 
 async function displayUserData(photographer) {
 	
+	// for connect to html element
 	const UserHeader = document.querySelector(".photograph-header");
+	// to connect to our factory function 
 	const photographerInfo = photographerFactory(photographer);
+	// to get our User data function
 	const UserProfile = photographerInfo.getProfilDom();
+
+	const UserPrice = document.querySelector(".pricePerDay")
+	const pricePerDay = document.createTextNode(UserHeader.price)
 
     // for insert user profil in html before contact button 
 	UserHeader.insertBefore(UserProfile, UserHeader.firstChild);
+
+	// for insert price in html before
+	UserPrice.insertBefore(pricePerDay, UserPrice.firstChild)
 
 }
 
@@ -27,13 +36,17 @@ function getUserId() {
 }
 
 async function init() {
+
 	// get data of photographer before load the other function 
 	const { photographers } = await getData();
 
 	// find photographer by params of the page 
 	const photographer = photographers.find(photographe => photographe.id == getUserId());
+	
     // for display data 
 	displayUserData(photographer);
 }
 
 init();
+
+
